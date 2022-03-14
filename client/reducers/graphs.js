8e5d10ae34e45bd2a18b9bef94f95258a33aacf5
samuel-wd-kirby <samkirby@outlook.com>
2022-03-14 20:47:24 +1300

    mock data

--- client/reducers/graphs.js
+++ client/reducers/graphs.js
@@ -1,6 +1,6 @@
 const initialState = { 
     Apples: [2,4, 6, 7 , 5, 4, 8 , 4, 3, 9, 4, 7], 
-    Peanuts: [ 2, 4 ] 
+    Peanuts: [ 2, 4, 6, 4, 4, 8, 3, 5 ] 
 }
 
 function graphs (state = initialState, action) { 
